037099614bc395484e1e20ef2b8cceed4da89fa7
ssabrut <michael.gunawan2002@gmail.com>
2022-08-05 15:37:02 +0700

    Create frontend for render page

--- frontend/src/pages/Render.vue
+++ frontend/src/pages/Render.vue
@@ -1,14 +1,47 @@
 <template>
-  <div class="container mx-auto">
+  <div class="container mx-auto mb-8">
     <form @submit.prevent="submit" enctype="multipart/form-data">
-      <input
-        @change="handleFileUpload"
-        type="file"
-        name="content_image"
-        ref="newCover"
-      />
-      <img src="" id="content_placeholder" />
-      <button type="submit">Submit</button>
+      <div class="text-end mb-4">
+        <button class="bg-blue-500 px-3 py-1 rounded-full text-white ring ring-blue-500 cursor-pointer hover:bg-blue-400 hover:ring-blue-400" type="submit">Create Art</button>
+      </div>
+      <div class="border rounded mb-8">
+        <div class="bg-gray-50 p-4 border-b">
+          <div class="flex justify-between items-center">
+            <p class="font-semibold">Content image</p>
+            <label class="bg-blue-500 px-3 py-1 rounded text-white ring ring-blue-500 cursor-pointer hover:bg-blue-400 hover:ring-blue-400" for="content_image">Upload</label>
+            <input
+              class="hidden"
+              @change="handleContentUpload"
+              type="file"
+              name="content_image"
+              id="content_image"
+              ref="contentCover"
+            />
+          </div>
+        </div>
+        <div class="my-6 mx-4 grid place-items-center" id="content_placeholder">
+          <p>You have no content photo uploaded, please upload your photo!</p>
+        </div>
+      </div>
+      <div class="border rounded">
+        <div class="bg-gray-50 p-4 border-b">
+          <div class="flex justify-between items-center">
+            <p class="font-semibold">Style image</p>
+            <label class="bg-blue-500 px-3 py-1 rounded text-white ring ring-blue-500 cursor-pointer hover:bg-blue-400 hover:ring-blue-400" for="style_image">Upload</label>
+            <input
+              class="hidden"
+              @change="handleStyleUpload"
+              type="file"
+              name="style_image"
+              id="style_image"
+              ref="styleCover"
+            />
+          </div>
+        </div>
+        <div class="my-6 mx-4 grid place-items-center" id="style_placeholder">
+          <p>You have no style photo uploaded, please upload your photo!</p>
+        </div>
+      </div>
     </form>
   </div>
 </template>
@@ -17,8 +50,10 @@
 import { onMounted, ref } from "vue";
 import axios from "axios";
 
-const newCover = ref(null);
-const realCover = ref(null);
+const contentCover = ref(null);
+const realContent = ref(null);
+const styleCover = ref(null);
+const realStyle = ref(null);
 
 onMounted(async () => {
   axios
@@ -31,14 +66,35 @@ onMounted(async () => {
     });
 });
 
-const handleFileUpload = () => {
+const handleContentUpload = () => {
   let reader = new FileReader();
-  if (newCover.value.files) {
-    if (/.(jpe?g|png|gif)$/i.test(newCover.value.files[0].name)) {
-      reader.readAsDataURL(newCover.value.files[0]);
-      realCover.value = newCover.value.files[0];
+  if (contentCover.value.files) {
+    if (/.(jpe?g|png|gif)$/i.test(contentCover.value.files[0].name)) {
+      reader.readAsDataURL(contentCover.value.files[0]);
+      realContent.value = contentCover.value.files[0];
       reader.onload = (e) => {
-        document.getElementById("content_placeholder").src = e.target.result;
+        const contentPlaceholder = document.getElementById("content_placeholder");
+        contentPlaceholder.removeChild(contentPlaceholder.firstChild);
+        const imagePlaceholder = document.createElement('img');
+        imagePlaceholder.classList.add('w-[384px]', 'h-[384px]', 'object-cover', 'shadow-lg', 'rounded');
+        contentPlaceholder.appendChild(imagePlaceholder).src = e.target.result;
+      };
+    }
+  }
+};
+
+const handleStyleUpload = () => {
+  let reader = new FileReader();
+  if (styleCover.value.files) {
+    if (/.(jpe?g|png|gif)$/i.test(styleCover.value.files[0].name)) {
+      reader.readAsDataURL(styleCover.value.files[0]);
+      realStyle.value = styleCover.value.files[0];
+      reader.onload = (e) => {
+        const stylePlaceholder = document.getElementById("style_placeholder");
+        stylePlaceholder.removeChild(stylePlaceholder.firstChild);
+        const imagePlaceholder = document.createElement('img');
+        imagePlaceholder.classList.add('w-[384px]', 'h-[384px]', 'object-cover', 'shadow-lg', 'rounded');
+        stylePlaceholder.appendChild(imagePlaceholder).src = e.target.result;
       };
     }
   }
@@ -46,7 +102,7 @@ const handleFileUpload = () => {
 
 const submit = async () => {
   const formData = new FormData();
-  formData.append("file", realCover.value);
+  formData.append("file", realContent.value);
   axios
     .post("http://localhost:3000/api/upload", formData, {
       headers: {
