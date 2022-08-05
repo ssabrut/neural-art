9c9c3de0ae1373b8cc68b1c17924e8e5f14adb20
ssabrut <michael.gunawan2002@gmail.com>
2022-08-05 13:12:40 +0700

    Add upload image

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,38 @@ const express = require("express");
 const app = express();
 const executeQuery = require("./db");
 const cors = require("cors");
+const multer = require("multer");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cors());
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: async (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({
+  storage,
+});
+
 app.get("/api/test", async (req, res) => {
   const results = await executeQuery("SELECT * FROM content_images");
   return res.status(200).send(results);
 })
+
+try {
+  app.post("/api/upload", upload.single('file'), async (req, res) => {
+    const results = await executeQuery("INSERT INTO content_images (image) VALUES (?)", [req.file.originalname]);
+    return res.status(200).download(`uploads/${req.file.originalname}`);
+  })
+} catch (error) {
+  return res.status(500).send(error.message);
+}
 
 app.listen(3000, () => {
   console.log("Listened to port 3000");
--- frontend/src/pages/Render.vue
+++ frontend/src/pages/Render.vue
@@ -1,14 +1,63 @@
 <template>
-  <h1>Render page</h1>
+  <div class="container mx-auto">
+    <form @submit.prevent="submit" enctype="multipart/form-data">
+      <input
+        @change="handleFileUpload"
+        type="file"
+        name="content_image"
+        ref="newCover"
+      />
+      <img src="" id="content_placeholder" />
+      <button type="submit">Submit</button>
+    </form>
+  </div>
 </template>
 
 <script setup>
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import axios from "axios";
 
+const newCover = ref(null);
+const realCover = ref(null);
+
 onMounted(async () => {
-  axios.get("http://127.0.0.1:3000/api/test").then((res) => {
-    console.log(res.data);
-  });
+  axios
+    .get("http://localhost:3000/api/test")
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((e) => {
+      console.error(e);
+    });
 });
+
+const handleFileUpload = () => {
+  let reader = new FileReader();
+  if (newCover.value.files) {
+    if (/.(jpe?g|png|gif)$/i.test(newCover.value.files[0].name)) {
+      reader.readAsDataURL(newCover.value.files[0]);
+      realCover.value = newCover.value.files[0];
+      reader.onload = (e) => {
+        document.getElementById("content_placeholder").src = e.target.result;
+      };
+    }
+  }
+};
+
+const submit = async () => {
+  const formData = new FormData();
+  formData.append("file", realCover.value);
+  axios
+    .post("http://localhost:3000/api/upload", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    })
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((e) => {
+      console.error(e);
+    });
+};
 </script>
